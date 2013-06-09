1ed67f03fcaef8fde12f89fa57ba3189a5565404
Sławomir Zarucki <zarucki@gmail.com>
2013-06-10 00:02:57 +0200

    some refactoring and beginings of labels for fields

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 dofile('utilities.lua')
 white = {255, 255, 255}
 black = {0, 0, 0}
+transparent = {0, 0, 0, 0}
+lablelTextColor = black
 
 function love.load()
   screen = {}
@@ -11,10 +13,14 @@ function love.load()
   board = {}
   board.width = minDimension / 2 - 20
   board.height = board.width
+  board.rankCount = 3
   board.outerCircleRatios = { 0.42, 0.65, 0.92 }
   board.numberOfFieldsOnCircles = 16
   board.fieldRadius = board.width / 16
+  board.files = { {'A', 'F'}, {'B', 'E'}, {'C', 'H'}, {'D', 'G'} }
+  board.labelCenterOffset = 35
 
+  love.graphics.setFont(love.graphics.newFont(10))
   love.graphics.setBackgroundColor(white)
 end
 
@@ -24,41 +30,34 @@ function love.conf(t)
 end
 
 function love.keypressed(key)
-  if key == "escape" then
+  if key == 'escape' then
     love.event.push('quit') -- Quit the game : <
   end
 end
 
 function love.draw()
-  for index, value in ipairs(board.outerCircleRatios) do
-    local currentCircle = Circle(screen.width/2, screen.height/2, value * board.width)
-    love.graphics.setColor(black)
-    love.graphics.circle("line", currentCircle.centerX, currentCircle.centerY, currentCircle.radius)
+  love.graphics.setColor(black)
+
+  for value = 1, board.rankCount do
+    local currentRankRadius = (board.outerCircleRatios[value] or 0) * board.width
+    local currentCircle = Circle(screen.width/2, screen.height/2, currentRankRadius)
+    local circleForLabels = Circle(screen.width/2, screen.height/2, currentRankRadius + board.labelCenterOffset)
+    love.graphics.FilledCircle(currentCircle, nil, black)
 
     local degreeStep = 360 / board.numberOfFieldsOnCircles
     local currentDegree = 0
 
     for i = 1, board.numberOfFieldsOnCircles do
       local x, y = currentCircle:GetPointByAngle(math.toRadians(currentDegree))
-      drawShapeWithContour(black, white, board.fieldRadius,
-        function (fillMode, radius)
-          love.graphics.circle(fillMode, x, y, radius)
-        end)
-      -- love.graphics.setColor(0, 0, 0)
-      -- love.graphics.circle("line", x, y, board.fieldRadius)
-      -- love.graphics.setColor(255, 255, 255)
-      -- love.graphics.circle("fill", x, y, board.fieldRadius - love.graphics.getLineWidth())
+      love.graphics.FilledCircle(Circle(x, y, board.fieldRadius), white, black)
+
+      local labelX, labelY = circleForLabels:GetPointByAngle(math.toRadians(currentDegree))
+      love.graphics.setColor(lablelTextColor)
+      local currentFont = love.graphics.getFont()
+      love.graphics.print('AB1', labelX - currentFont:getWidth('AB1') / 2, labelY, math.toRadians(currentDegree + 90), 1)
       currentDegree = currentDegree + degreeStep
     end
   end
-end
-
-function drawShapeWithContour(contourColor, fillColor, radius, drawShapeFunction)
-  love.graphics.setColor(contourColor)
-  drawShapeFunction("line", radius)
-
-  love.graphics.setColor(fillColor)
-  drawShapeFunction("fill", radius - love.graphics.getLineWidth())
 end
 
 function math.toRadians(angleInDegrees)
--- utilities.lua
+++ utilities.lua
@@ -1,6 +1,10 @@
 Circle = {}
 Circle.__index = Circle
-setmetatable(Circle, { __call = function(_, ...) return Circle.new(...) end })
+setmetatable(Circle, {
+  __call = function(_, ...)
+    return Circle.new(...)
+  end
+})
 
 function Circle.new(x, y, radius)
   local newObject = {}
@@ -15,4 +19,22 @@ function Circle:GetPointByAngle(angleInDegrees)
   local newX = self.centerX + self.radius * math.cos(angleInDegrees)
   local newY = self.centerY + self.radius * math.sin(angleInDegrees)
   return newX, newY
+end
+
+function Circle:__tostring()
+  return 'centerX: ' .. self.centerX .. ', centerY: ' .. self.centerY .. ', radius: ' .. self.radius
+end
+
+function love.graphics.FilledCircle(circle, fillColor, countourColor)
+  local circleRadius = circle.radius
+  if countourColor then
+    love.graphics.setColor(countourColor)
+    love.graphics.circle("line", circle.centerX, circle.centerY, circleRadius)
+    circleRadius = circleRadius - love.graphics.getLineWidth()
+  end
+
+  if fillColor then
+    love.graphics.setColor(fillColor)
+    love.graphics.circle("fill", circle.centerX, circle.centerY, circleRadius)
+  end
 end
